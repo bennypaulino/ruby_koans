1385e60012e3e6d4a1f788b0d5862c5000e15b69
bennypaulino <santiagopro71@gmail.com>
2017-04-19 18:06:37 -0400

    Complete about_triangle_project2.rb

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  sides = [a, b, c].sort
+
+  raise TriangleError, 'all sides must be > 0' if sides.reduce(:+) <= 0
+  raise TriangleError, 'a side cannot be negative' if sides.any?(&:negative?)
+  raise TriangleError, 'sum of 2 smaller sides must be > 3rd side' unless (sides[0] + sides[1]) > sides.max
+
   if (a == b) && (b == c)
     return :equilateral
   elsif (a == b) || (a == c) || (b == c)
